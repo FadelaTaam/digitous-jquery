11f3fc69238aede612efbe89d12c5ed01ea9b766
unknown <taam-fadela@live.com>
2021-05-05 14:49:42 +0200

    exo 1 jquery

---
+++ exercice1/js/main.js
@@ -0,0 +1,10 @@
+
+
+$(document).ready(function () {
+    // tout le reste du code viendra ici
+    $("#exercise").hover(function () {
+        $("#square").css({ "background-color": "red", "height": "500px" });
+    })
+
+
+});
