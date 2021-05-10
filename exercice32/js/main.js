e36ff08ea76054caaafab254d43112472bbed80f
FadelaTaam <taam-fadela@live.com>
2021-05-10 17:05:08 +0200

    exo 32

---
+++ exercice32/js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+    $("button").click(function () {
+        $.ajax({
+            method: "GET",
+            url: "https://restcountries.eu/rest/v2/name/france",
+            success: function (data, statuts, response) {
+
+                console.log("data", data[0].capital, data[0].name);
+        
+
+            }
+        });
+    });
+    
+});
+/* $(data[0].capital, data[0].name).val(); */
